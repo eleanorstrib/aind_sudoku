e4805e3b154bb83686132e61131f9f8fc1adf5a7
eleanorstrib <hello@eleanorstrib.com>
2017-12-17 22:28:15 -0800

    fixed previous error but have new ones for diag and naked twins

--- solution.py
+++ solution.py
@@ -7,7 +7,10 @@ square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','45
 unitlist = row_units + column_units + square_units
 
 # TODO: Update the unit list to add the new diagonal units
-unitlist = unitlist + [rows[i] + cols[i] for i in range(len(rows))] + [rows[i] + cols[i] for i in range(len(rows) -1, -1, -1)]
+diagonal_lr = [rows[i] + cols[i] for i in range(len(rows))]
+diagonal_rl = [rows[i] + cols[i] for i in range(len(rows) -1, -1, -1)]
+unitlist.append(diagonal_lr)
+unitlist.append(diagonal_rl)
 
 units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
 peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
