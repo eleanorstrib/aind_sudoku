6c3f27c2d4ca70e56b6d39fb824d6f9a4248d028
eleanorstrib <hello@eleanorstrib.com>
2017-12-17 16:57:28 -0800

    Think I have the functions covered in the lessons copied in/modified correctly

--- solution.py
+++ solution.py
@@ -1,7 +1,6 @@
 
 from utils import *
 
-
 row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
@@ -42,7 +41,10 @@ def naked_twins(values):
     strategy repeatedly).
     """
     # TODO: Implement this function!
-    raise NotImplementedError
+    # for u in unitlist: # search all units: col, box, row
+    #     print (u)
+    return values
+    # raise NotImplementedError
 
 
 def eliminate(values):
@@ -61,8 +63,26 @@ def eliminate(values):
     dict
         The values dictionary with the assigned values eliminated from peers
     """
-    # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    # TODO: Copy your code from the classroom and modify it to complete this function
+    single_values = [box for box in values.keys() if len(values[box]) == 1]
+
+    for box in single_values:
+        for p in peers[box]:
+            values[p] = values[p].replace(values[box], '')
+    return values
+        # for box in peer_list:
+        #     if len(values[box]) > 1:
+
+        # for p in peer_list:
+        #     if len(values[p]) > 1:
+
+
+                # remove that value from every other item in the dict in peer list
+                 # = {k:('hi' if v=='.' else v) for (k,v) in d.items()}
+    # print(values)
+    # d = dict(zip(boxes, grid))
+    # return {k:('123456789' if v=='.' else v) for (k,v) in d.items()}
+    # raise NotImplementedError
 
 
 def only_choice(values):
@@ -85,8 +105,15 @@ def only_choice(values):
     -----
     You should be able to complete this function by copying your code from the classroom
     """
-    # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    # TODO: Copy your code from the classroom and modify it to complete this function
+    for u in unitlist: # go through units (list of boxes)
+        for d in '123456789': # look through each digit in the list
+            dpl = [box for box in u if d in values[box]] # for each digit, make a list of boxes that has digit
+            if len(dpl) == 1: # if the length of the list of boxes w a digit is 1
+                values[dpl[0]] = d  # change the value of the box to that digit
+
+    return values
+    # raise NotImplementedError
 
 
 def reduce_puzzle(values):
@@ -101,10 +128,28 @@ def reduce_puzzle(values):
     -------
     dict or False
         The values dictionary after continued application of the constraint strategies
-        no longer produces any changes, or False if the puzzle is unsolvable 
+        no longer produces any changes, or False if the puzzle is unsolvable
     """
     # TODO: Copy your code from the classroom and modify it to complete this function
-    raise NotImplementedError
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+
+        # Your code here: Use the Eliminate Strategy
+        eliminate(values)
+
+        # Your code here: Use the Only Choice Strategy
+        only_choice(values)
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
+    # raise NotImplementedError
 
 
 def search(values):
@@ -127,7 +172,23 @@ def search(values):
     and extending it to call the naked twins strategy.
     """
     # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+        # First, reduce the puzzle using the previous function
+    values = reduce_puzzle(values)
+    if values is False:
+        return False
+    # check if it's solved e.g. length of all values in boxes is 1
+    if all(len(values[b]) == 1 for b in boxes):
+        return values
+    # Choose one of the unfilled squares with the fewest possibilities
+    v_len, box = min((len(values[s]), s) for s in boxes if len(values[s]) > 1) #tuple with len + box id
+    # # Now use recursion to solve each one of the resulting sudokus, and if one returns a value (not False), return that answer!
+    for value in values[box]:
+        new_sudoku = values.copy() # copy the puzzle
+        new_sudoku[box] = value # value is
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
+    # raise NotImplementedError
 
 
 def solve(grid):
@@ -137,7 +198,7 @@ def solve(grid):
     ----------
     grid(string)
         a string representing a sudoku grid.
-        
+
         Ex. '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
 
     Returns
